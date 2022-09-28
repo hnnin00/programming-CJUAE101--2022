aa0d724aeae8001da7652a11a27c122927cac5db
학민 <h_nnin0827@naver.com>
2022-09-28 09:37:24 +0900

    2주차 첫번쨰 과제입니다.
    2주차 첫번째 과제입니다.

---
+++ 2ndWeek/Prob_1/Prob_1/main.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char ch1 = 'A';
+	char ch2 = 65;
+	char ch3 = 't';
+	char ch4 = 116;
+
+	printf("Enter characters : %c\nA", ch1, ch1);
+	printf("The ASCII code value of the character is %d.\n", ch2, ch2);
+	printf("Enter characters : %c\n", ch3, ch3);
+	printf("tThe ASCII code value of the character is %d.", ch4, ch4);
+
+	return 0;
+}
